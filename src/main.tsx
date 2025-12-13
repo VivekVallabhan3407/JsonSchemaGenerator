a689b84ccbd0c6785313b3620e6c1fa3e4e8bfa6
VivekVallabhan3407 <vvivek.1804@gmail.com>
2025-12-13 22:28:30 +0530

    updated main.jsx to import  react-toast notify taost container other related files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './styles/global.css'
 
 createRoot(document.getElementById('root')!).render(
