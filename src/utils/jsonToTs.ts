076720bb40563306284a943603f697d3168e3e2d
VivekVallabhan3407 <vvivek.1804@gmail.com>
2025-12-13 14:02:10 +0530

    updated JsonToTs.ts

--- src/utils/jsonToTs.ts
+++ src/utils/jsonToTs.ts
@@ -1,37 +1,34 @@
 // utils/jsonToTs.ts
 
-export const convertJsonToTs = (jsonInput: string) => {
-  try {
-    const json = JSON.parse(jsonInput);
+export const convertJsonToTs = (json: any): string => {
 
-    const generateTS = (value: any): string => {
-      if (Array.isArray(value)) {
-        if (value.length === 0) return "any[]";
-        return `${generateTS(value[0])}[]`;
-      }
+  const generateTS = (value: any): string => {
+    if (Array.isArray(value)) {
+      if (value.length === 0) return "any[]";
+      return `${generateTS(value[0])}[]`;
+    }
 
-      switch (typeof value) {
-        case "string":
-          return "string";
-        case "number":
-          return "number";
-        case "boolean":
-          return "boolean";
-        case "object":
-          if (value === null) return "null";
+    if (value === null) return "null";
 
-          let fields = "";
-          for (const key in value) {
-            fields += `  ${key}: ${generateTS(value[key])};\n`;
-          }
-          return `{\n${fields}}`;
-        default:
-          return "any";
-      }
-    };
 
-    return `interface RootObject ${generateTS(json)}`;
-  } catch {
-    return null;
-  }
+    switch (typeof value) {
+      case "string":
+        return "string";
+      case "number":
+        return "number";
+      case "boolean":
+        return "boolean";
+      case "object":
+
+        let fields = "";
+        for (const key in value) {
+          fields += `  ${key}: ${generateTS(value[key])};\n`;
+        }
+        return `{\n${fields}}`;
+      default:
+        return "any";
+    }
+  };
+
+  return `interface RootObject ${generateTS(json)}`;
 };
