d3e8c2863cd89080414dd02918ef15be2d7a417f
VivekVallabhan3407 <vvivek.1804@gmail.com>
2025-12-13 22:44:44 +0530

    updated JsonInput.tsx to add import statement for react-toast notify toast messages

--- src/components/JsonInput/JsonInput.tsx
+++ src/components/JsonInput/JsonInput.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {toast} from 'react-toastify';
 import "./JsonInput.css";
 import { useStateJson } from "@/hooks/useStateJson";
 import { validateJson } from "@/utils/validateJson";
